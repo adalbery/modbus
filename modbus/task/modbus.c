ff18dd581b771facbeee1e312a30d8d4953846ce
AdalberyCastro <adalbery@ufpa.br>
2022-01-09 11:48:57 -0300

    Modbus funcoes 01, 03 e 16. ATSAMV71Q21B. SIMA02

--- modbus/task/modbus.c
+++ modbus/task/modbus.c
@@ -3,13 +3,15 @@
  *
  * Created: 17/12/2021 15:03:33
  *  Author: Adalbery
- * Microcontrolador ATSAMV71Q21B - CORTEX M7
- * Modbus utiliza USART0. (RXD = PB0 - TXD = PB1 - RTS = PB3)
- * Renomear PB3 para RTS_RS485 no Atmel Start
+ * Projeto SIMA.
+ * Programa para implementar protocolo Modbus utilizando microcontrolador ATSAMV71Q21B - CORTEX M7
+ * Testes realizado com o kit SAM V71 Xplained Ultra.
+ * Modbus utiliza USART0: RXD = PB0 - TXD = PB1 - RTS = PB3.
+ * Renomear PB3 para RTS_RS485 no Atmel Start.
  * Implementa as funcoes do modbus:
- * 03 (0x03) Read Holding Registers
- * 04 (0x04) Read Input Registers
- * 16 (0x10) Write Multiple registers
+ * 03 (0x03) Read Holding Registers.
+ * 04 (0x04) Read Input Registers.
+ * 16 (0x10) Write Multiple registers.
  */ 
 
 #include <atmel_start.h>
